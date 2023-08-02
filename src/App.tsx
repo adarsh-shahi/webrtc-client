d51fb30999fe743b7fe5539bd72421111f05f4bb
Adarsh Shahi <adarshshahi1009@gmail.com>
2023-08-02 23:20:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-	return <div className="bg-orange-700">hey there</div>;
+	return <div className="bg-orange-500">hey there</div>;
 }
 
 export default App;
